64f1017b09f3b341b75b3cb1e97d939356221efe
Akashdeep <akashdeep@active.agency>
2022-11-30 18:00:41 +0530

    conditional statements with lists

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
 
-  const expenses = [
+  const DUMMY_EXPENSES = [
     {
       id: "e1",
       title: "Toilet Paper",
@@ -10,22 +11,25 @@ function App() {
       date: new Date(2021, 5, 12)
     },
     {
-      id: "e1",
+      id: "e2",
       title: "Car Insurance",
       amount: "286",
       date: new Date(2022, 5, 12)
     },
     {
-      id: "e1",
+      id: "e3",
       title: "New Desk(Wooden)",
       amount: "286",
-      date: new Date(2022, 7, 2)
+      date: new Date(2020, 7, 2)
     }
   ];
 
+  const [expenses, setExpense] = useState(DUMMY_EXPENSES)
+
   const addExpenseHadler = (expense) => {
-    console.log("In app.js");
-    console.log(expense);
+    setExpense((preExpenses) => {
+      return [expense, ...preExpenses];
+    })
   }
 
   return (
